7b89f96793906f46653c4ab9460a62d74c13e20a
wangyafe <2085374271@qq.com>
2019-08-13 17:20:05 +0800

    test

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package demo;
+
+/**
+ * Created by Bell on 2019/8/13.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("made it!");
+    }
+}
