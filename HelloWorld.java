18116d9b28598dccd80390c17a88631f3f289242
王亚飞 <wb-wyf813546@antgroup.com>
2020-09-29 17:34:03 +0800

    压缩测试

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("still alive");
         System.out.println("sldw");
+        System.out.println("kdfjkj");
+        System.out.println("kdjfkdj");
     }
 }
